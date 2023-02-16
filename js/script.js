02f627f8b7f12a82315a74697b04c03231b5ab8a
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-02-16 11:15:10 +0100

    Add content in new file eslintrc.json

--- js/script.js
+++ js/script.js
@@ -1,63 +1,61 @@
-{
-  const titleClickHandler = function (event) {
-    event.preventDefault();
-    const clickedElement = this;
-    console.log("Link was clicked!");
-    console.log(event);
+const titleClickHandler = function (event) {
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
+  console.log(event);
 
-    const activeLinks = document.querySelectorAll(".titles a.active");
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for (let activeLink of activeLinks) {
-      activeLink.classList.remove("active");
-    }
-
-    console.log("clickedElement:", clickedElement);
-    clickedElement.classList.add("active");
-
-    const activeArticles = document.querySelectorAll(".posts article.active");
-
-    for (let activeArticle of activeArticles) {
-      activeArticle.classList.remove("active");
-    }
-
-    const articleSelector = clickedElement.getAttribute("href");
-
-    const targetArticle = document.querySelector(articleSelector);
-
-    targetArticle.classList.add("active");
-  };
-
-  const optArticleSelector = ".post",
-    optTitleSelector = ".post-title",
-    optTitleListSelector = ".titles";
-
-  function generateTitleLinks() {
-    const titleList = document.querySelector(optTitleListSelector);
-    titleList.innerHTML = "";
-
-    const articles = document.querySelectorAll(optArticleSelector);
-    let html = "";
-    for (let article of articles) {
-      const articleId = article.getAttribute("id");
-      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-
-      const linkHTML =
-        '<li><a href="#' +
-        articleId +
-        '"><span>' +
-        articleTitle +
-        "</span></a></li>";
-
-      titleList.insertAdjacentHTML("beforebegin", linkHTML);
-      html = html + linkHTML;
-    }
-    //titleList.innerHTML = html;
-    const links = document.querySelectorAll(".sidebar a");
-
-    for (let link of links) {
-      link.addEventListener("click", titleClickHandler);
-    }
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
   }
 
-  generateTitleLinks();
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
+
+  const articleSelector = clickedElement.getAttribute('href');
+
+  const targetArticle = document.querySelector(articleSelector);
+
+  targetArticle.classList.add('active');
+};
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for (let article of articles) {
+    const articleId = article.getAttribute('id');
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    const linkHTML =
+      '<li><a href="#' +
+      articleId +
+      '"><span>' +
+      articleTitle +
+      '</span></a></li>';
+
+    titleList.insertAdjacentHTML('beforebegin', linkHTML);
+    html = html + linkHTML;
+  }
+  //titleList.innerHTML = html
+  const links = document.querySelectorAll('.sidebar a');
+
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
 }
+
+generateTitleLinks();
