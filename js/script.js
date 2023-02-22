645cab3d1ec1730684f4f55e9b3a7a04f3d36ce9
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-02-22 15:48:14 +0100

    Add fix in function generateTags and add content in function generateAuthors in script.js

--- js/script.js
+++ js/script.js
@@ -31,6 +31,8 @@ const optArticleSelector = '.post',
   optTitleListSelector = '.titles';
 optArticleTagsSelector = '.post-tags .list';
 optTagsListSelector = '.tags.list';
+optArticleAuthorSelector = '.post-author';
+optAuthorListSelector = '.authors.list';
 
 function generateTitleLinks(customSelector = '') {
   const titleList = document.querySelector(optTitleListSelector);
@@ -69,6 +71,7 @@ function generateTags() {
 
   for (let article of articles) {
     const tagsWrapper = article.querySelector(optArticleTagsSelector);
+    tagsWrapper.innerHTML = '';
     let html = '';
     const articleTags = article.getAttribute('data-tags');
     const articleTagsArray = articleTags.split(' ');
@@ -96,7 +99,7 @@ function generateTags() {
       activeTagLink.classList.remove('active');
     }
 
-    const tagLinks = document.querySelector(href);
+    const tagLinks = document.querySelectorAll(href);
 
     for (let tagLink of tagLinks) {
       tagLink.classList.add('active');
@@ -108,10 +111,58 @@ function generateTags() {
     const links = document.querySelectorAll('.list a');
 
     for (let link of links) {
-      link.addEventListener('click', titleClickHandler);
+      link.addEventListener('click', tagClickHandler);
     }
   }
   addClickListenersToTags();
 }
 
 generateTags();
+
+function generateAuthors() {
+  let allAuthors = [];
+  const articles = document.querySelectorAll(optArticleSelector);
+  for (let article of articles) {
+    const authorsWrapper = article.querySelector(optArticleAuthorSelector);
+    let html = '';
+    const author = article.getAttribute('data-author');
+    const linkHTML = '<a href="#"><span>' + author + '</span></a>';
+    html = html + linkHTML;
+    if (allAuthors.indexOf(linkHTML) == -1) {
+      allAuthors.push(linkHTML);
+    }
+
+    authorsWrapper.innerHTML = html;
+  }
+  const authorList = document.querySelector(optAuthorListSelector);
+  authorList.innerHTML = allAuthors.join(' ');
+
+  function authorClickHandler(event) {
+    event.preventDefault();
+    const clickedElement = this;
+
+    const activeAuthorLinks = document.querySelectorAll('.authors a');
+
+    for (let activeAuthorLink of activeAuthorLinks) {
+      activeAuthorLink.classList.remove('active');
+    }
+
+    clickedElement.classList.add('active');
+    //const authorLinks = document.querySelectorAll(href);
+
+    //for (let authorLink of authorLinks) {
+    //  authorLink.classList.add('active');
+    //}
+    generateTitleLinks('[data-author="' + author + '"]');
+  }
+
+  function addClickListenersToAuthors() {
+    const links = document.querySelectorAll('.post-author a');
+
+    for (let link of links) {
+      link.addEventListener('click', authorClickHandler);
+    }
+  }
+  addClickListenersToAuthors();
+}
+generateAuthors();
